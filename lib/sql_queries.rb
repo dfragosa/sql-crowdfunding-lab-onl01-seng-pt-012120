bb949316609d729ce927be10d676d1ced39770f6
Derric Fragosa <fderric@gmail.com>
2020-03-29 06:35:38 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -54,6 +54,6 @@ def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_b
   "SELECT projects.category, SUM(pledges.amount)
   FROM projects
   JOIN pledges
-  ON 
+  ON projects.id = pledges.projects_id
   WHERE projects.category = 'books'; "
 end
